e03a3a2dfa58d50ecd851d017378e93bb6d4428e
zidane-arkan <zidark897@Gmail.com>
2022-08-10 05:33:35 +0700

    update 1.0

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Main from './components/Layout/Main';
+function App() {
+  return (
+    <div>
+      <Main></Main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import headerClass from './Header.module.css';
+
+function Header() {
+    return (
+        <div>
+            <navbar className={headerClass.header}>
+
+            </navbar>
+        </div>
+    );
+}
+
+export default Header;
