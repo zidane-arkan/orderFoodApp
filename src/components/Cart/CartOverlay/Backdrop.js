3e5282edb021445f57c1a956e6cb8f27e78c1f7b
zidane-arkan <zidark897@Gmail.com>
2022-08-12 08:29:58 +0700

    Add Cart & Backdrop Component

---
+++ src/components/Cart/CartOverlay/Backdrop.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import BackdropClass from './Backdrop.module.css';
+
+function Backdrop() {
+  return (
+    <section className={BackdropClass.backdrop}/>
+  )
+}
+
+export default Backdrop;
---
+++ src/components/Cart/CartOverlay/CartModal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CartModalClass from './CartModal.module.css';
+
+function CartOverlay() {
+    return (
+        <section className={CartModalClass.modal}>
+            <div></div>
+        </section>
+    );
+}
+
+export default CartOverlay;
